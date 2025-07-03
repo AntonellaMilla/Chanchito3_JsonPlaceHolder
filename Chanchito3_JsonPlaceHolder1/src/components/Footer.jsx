cf0a9376bab6fbaff6d26dc6daac10c0d6e0b1e0
AntonellaMilla <antonella.milla@tecsup.edu.pe>
2025-07-02 20:05:15 -0500

    Chanchito3_JsonPlaceHolder1

--- Chanchito3_JsonPlaceHolder1/src/components/Footer.jsx
+++ Chanchito3_JsonPlaceHolder1/src/components/Footer.jsx
@@ -3,16 +3,16 @@ import { usePostStore } from "../store/posts.store";
 import CardList from "./CardList";
 
 const Home = () => {
-  const posts = usePostStore(state => state.posts);
-  const getPosts = usePostStore(state => state.getPosts);
+  const posts = usePostStore((state) => state.posts);
+  const getPosts = usePostStore((state) => state.getPosts);
 
   useEffect(() => {
     getPosts();
-  }, []);
+  }, [getPosts]); // Dependencia explícita
 
   return (
     <section className="container my-4">
-      <h2>Últimos posts del blog</h2>
+      <h2 className="mb-4"></h2>
       <CardList data={posts} />
     </section>
   );
